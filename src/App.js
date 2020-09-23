0f35a04b8e31024791d66ea76fcf444636547a78
mark1227 <mark122787@gmail.com>
2020-09-24 01:14:59 +0800

    update

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './act1.css';
+import { render } from '@testing-library/react';
+
+class App extends Component {
+
+  state = { username: "Mako" };
+
+  UpdateUsername = ( event ) => {
+      this.setState({ username: event.target.value })
+  };
+
+  render(){
+
+    const design = {
+      border: 'solid blue 3px',
+      width: '60%',
+      // textalign: 'center',
+      font: 'verdana',
+      margin: '20px auto',
+      padding: '20px',
+      // boxshadow: '10px 5px 5px cornflowerblue'
+    };
+
+    return (
+      <div style={design}>
+      <UserInput changed = { this.UpdateUsername } name = {this.state.username}/>
+      <UserOutput name= { this.state.username }  />
+      </div>
+    );
+  }
+}
+
+const UserInput = ( props ) =>{
+  return (
+    <div className="In">
+      <input type="text" 
+        onChange = { props.changed }
+      />
+    </div>
+  );
+}
+
+const UserOutput = (props) =>{
+  return (
+    <div class="App">
+      <p>Your username is { props.name }</p>
+      <p>And you are Gwapo </p>
+    </div>
+  );
+}
+
+export default App;
